62b142400b85db083749c76f462eae28be5e3644
César A Ojeda Escobar <cesararleyoe@Cesars-MacBook-Air.local>
2023-01-26 18:00:28 -0600

    Added documentation view

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Video Service API')
+    .setDescription('YoContigo video service API for OpenVidu')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.enableCors();
 
@@ -13,8 +23,8 @@ async function bootstrap() {
       whitelist: true,
       forbidNonWhitelisted: true,
     }
-  ));
-  
+    ));
+
   const configService = app.get(ConfigService);
 
   await app.listen(configService.get<number>("http.port"), configService.get<string>("http.host"));
