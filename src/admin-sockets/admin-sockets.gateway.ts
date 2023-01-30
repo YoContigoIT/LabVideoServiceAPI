e06929fbdc7d0ca888a290ab66492bdaa92d66d9
César A Ojeda Escobar <cesararleyoe@Cesars-MacBook-Air.local>
2023-01-30 11:39:14 -0600

    Added subscription to admin dashboard

--- src/admin-sockets/admin-sockets.gateway.ts
+++ src/admin-sockets/admin-sockets.gateway.ts
@@ -4,6 +4,7 @@ import { Server, Socket } from 'socket.io';
 import { AdminSocketsService } from './admin-sockets.service';
 import { AgentsConnectionService } from 'src/agents-connection/agents-connection.service';
 import { GuestsConnectionService } from 'src/guests-connection/guests-connection.service';
+import { combineLatest, distinctUntilChanged, forkJoin, last, map } from 'rxjs';
 
 
 @WebSocketGateway()
@@ -41,6 +42,28 @@ export class AdminSocketsGateway implements OnGatewayConnection, OnGatewayDiscon
       .subscribe((priorityLine) => {
         client.emit('priority-line-update', priorityLine);
       });
+  }
+
+  @SubscribeMessage('register-to-dashboard-updates')
+  registerToDashboardUpdates(@ConnectedSocket() client: Socket) {
+
+    combineLatest([
+      this.agentsConnectionsService.rooms$,
+      this.guestConnectionsService.priorityLine$
+    ])
+      .pipe(map(data => {
+        return {
+          rooms: {
+            active: data[0].length,
+            inCall: data[0].filter(room => !room.available).length,
+            free: data[0].filter(room => room.available).length
+          },
+          priorityLine: data[1].length,
+        }
+      }))
+      .subscribe(data => {        
+        client.emit('dashboard-update', data);
+      })
   }
 
 }
