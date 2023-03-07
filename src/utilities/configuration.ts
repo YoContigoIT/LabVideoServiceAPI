5870335fc56085a405ab4962cab33f608f3d6acf
Josue Vargas <josue07vargas95@gmail.com>
2023-03-07 13:22:19 -0600

    Changed settings files to unique

--- src/utilities/configuration.ts
+++ src/utilities/configuration.ts
@@ -1,6 +1,3 @@
-// import { readFileSync } from "fs";
-// import * as yaml from "js-yaml";
-import { join } from "path";
-// import enviroment from "./env/env.interface";
+import settings from './env/settings.env';
 
-export default () => require(join(__dirname, `./env/${process.env.NODE_ENV}.env`)).default;
+export default () => settings;
