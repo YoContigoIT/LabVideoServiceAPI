4ffa68a54d386c31c41b96273e937302c26e7697
César A Ojeda Escobar <cesararleyoe@ip-192-168-1-3.ec2.internal>
2023-04-05 00:13:37 -0600

    load balancer check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
-import { Response } from 'express';
+import { Controller, Get, HttpStatus, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,7 +7,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  initialPage(@Res() response: Response) {
+  initialPage(@Res() response: Response, @Req() request: Request) {
+    console.log(request.headers['user-agent']);
+    if(request.headers['user-agent'] === "ELB-HealthChecker/") {
+      return response.status(HttpStatus.OK).send('Video Service API OK!');
+    }
+    
     return response.status(HttpStatus.I_AM_A_TEAPOT).redirect("https://fgebc.gob.mx");
   }
 }
