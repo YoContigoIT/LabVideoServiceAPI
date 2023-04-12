e9d2bb84bb28b1045f5f846bf94b5d9364337433
César A Ojeda Escobar <cesararleyoe@192.168.1.5>
2023-04-11 23:30:35 -0600

    Fixes

--- src/services/aws/aws.service.ts
+++ src/services/aws/aws.service.ts
@@ -1,4 +1,4 @@
-import { ListObjectsV2Command, DeleteObjectCommand, GetObjectCommand, PutObjectCommand, PutObjectCommandInput, PutObjectRequest, S3, S3Client, HeadObjectCommand } from '@aws-sdk/client-s3';
+import { ListObjectsV2Command, DeleteObjectCommand, GetObjectCommand, PutObjectCommand, PutObjectCommandInput, S3Client, HeadObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
